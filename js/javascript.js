7705cddb3f29c61fcd2602a316be1af1d03d7965
Bhavana <katakam.bhavana@gmail.com>
2022-02-25 10:02:46 +0530

    added ui

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,6 @@
+let count = 0;
+let score = 0;
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissor'];
     let choice = Math.floor(Math.random()*choices.length);
@@ -8,22 +11,28 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    // rock > scisscor > paper
     if (playerSelection == computerSelection) {
-        return `${playerSelection} and ${computerSelection} are equal so Tie..`;
+        return `${playerSelection} and ${computerSelection} are equal so Tie.. and score is ${score}`;
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock' ||
         playerSelection == 'scissor' && computerSelection == 'paper' ||
         playerSelection == 'rock' && computerSelection == 'scissor') {
-        return `${playerSelection} beats ${computerSelection} You win!!`;
+        score +=1;
+        return `${playerSelection} beats ${computerSelection} You win!! and score is ${score}`;
     }
     else {
-        return `${playerSelection} can't beat ${computerSelection} You Lose..`;
+        return `${playerSelection} can't beat ${computerSelection} You Lose.. and score is ${score}`;
     }
+
 }
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('please enter rock, paper or scissor');
-        console.log(playRound(playerSelection, computerPlay()));
-    }
-}
+
+Array.from(document.querySelectorAll('button')).forEach(
+    btn => btn.addEventListener('click', e => {
+        count += 1;
+        document.querySelector('div').innerText = playRound(e.target.innerHTML, computerPlay());
+        if(count == 5) {
+            document.querySelector('div').innerText = (score > 2)?`score is ${score} You win!!`:`score is ${score} You Lose!!`;
+            score = 0;
+            count = 0;
+        }
+    }));
